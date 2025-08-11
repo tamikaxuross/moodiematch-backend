aee22179eeb4b168d0c0e6165ccabf81ff624a59
Tamika Ross <tamika.ross.xu@gmail.com>
2025-08-11 16:47:18 -0500

    fixed CORS middleware order preventing favorites API calls

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const userRoutes = require("./routes/users");
-const quizRoutes = require("./routes/quiz");
-const favoritesRoutes = require("./routes/favorites");
-
-app.use("/api/favorites", favoritesRoutes)
-//const favoritesRoutes = require("./routes/favorites");
-//app.use("/api/favorites", favoritesRoutes);
-
 app.use(cors({
   origin: ["http://localhost:5173", "https://moodiematch-frontend.onrender.com"],
   methods: ["GET", "POST", "OPTIONS"],
@@ -19,8 +11,27 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Received:", req.method, req.originalUrl);
+  next();
+});
+
+const userRoutes = require("./routes/users");
+const quizRoutes = require("./routes/quiz");
+const favoritesRoutes = require("./routes/favorites");
+
 app.use("/api/users", userRoutes);
 app.use("/api/quiz", quizRoutes);
+app.use("/api/favorites", favoritesRoutes)
+//const favoritesRoutes = require("./routes/favorites");
+//app.use("/api/favorites", favoritesRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("MoodieMatch backend is running ");
+});
+
+
 
 app.get("/api/ping", (req, res) => {
   res.json({ message: "pong from Express + Postgres" });
@@ -28,14 +39,6 @@ app.get("/api/ping", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  console.log("Received:", req.method, req.originalUrl);
-  next();
-});
-
-app.get("/", (req, res) => {
-  res.send("MoodieMatch backend is running ");
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
